bc794ac592e992d2583ced7cbafa9bb0517190ec
zhouhong07 <zhouhong07@58ganji.com>
2019-09-03 19:56:20 +0800

    test

--- static/workboxServiceWorker.js
+++ static/workboxServiceWorker.js
@@ -2,7 +2,7 @@
  * @Author: zhouhong07
  * @Date: 2019-09-03 18:01:11
  * @LastEditors: 
- * @LastEditTime: 2019-09-03 19:47:17
+ * @LastEditTime: 2019-09-03 19:56:07
  * @Description: file content
  */
 /**
@@ -21,7 +21,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "static/precache-manifest.3277778a04fe07b218be2e650e681aec.js"
+  "/precache-manifest.3277778a04fe07b218be2e650e681aec.js"
 );
 
 workbox.core.skipWaiting();
