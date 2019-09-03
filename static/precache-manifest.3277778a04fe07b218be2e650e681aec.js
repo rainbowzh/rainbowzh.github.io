37377befbbc7c26878ca61878becb39c27cd36e9
zhouhong07 <zhouhong07@58ganji.com>
2019-09-03 18:07:34 +0800

    Ceshi

---
+++ static/precache-manifest.3277778a04fe07b218be2e650e681aec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f053ed77ec12ae89f1ee73d64150b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d228cbbfc64cc5990a38",
+    "url": "/static/css/main.chunk.css"
+  },
+  {
+    "revision": "522495f02ebf19b4441d",
+    "url": "/static/js/2.chunk.js"
+  },
+  {
+    "revision": "d228cbbfc64cc5990a38",
+    "url": "/static/js/main.chunk.js"
+  },
+  {
+    "revision": "ca44c5f3ac323e9a2b3a",
+    "url": "/static/js/runtime~main.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.svg"
+  }
+]);
