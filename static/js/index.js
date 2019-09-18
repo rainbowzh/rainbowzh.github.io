029ae90f16a025e03c91f47d83bc515c28069769
zhouhong07 <zhouhong07@58ganji.com>
2019-09-18 17:18:54 +0800

    fix

--- static/js/index.js
+++ static/js/index.js
@@ -2,10 +2,10 @@
  * @Author: zhouhong07
  * @Date: 2019-09-18 16:45:45
  * @LastEditors: 
- * @LastEditTime: 2019-09-18 17:15:53
+ * @LastEditTime: 2019-09-18 17:18:46
  * @Description: file content
  */
-import * as serviceWorker from './serviceWorker';
+import serviceWorker from './serviceWorker';
 
 console.log('innnnn');
 serviceWorker.register();
