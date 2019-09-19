453f86facb0fc50efa1c295e5e871762b61851bc
zhouhong07 <zhouhong07@58ganji.com>
2019-09-19 18:36:04 +0800

    fix

--- static/workboxServiceWorker.js
+++ static/workboxServiceWorker.js
@@ -2,7 +2,7 @@
  * @Author: zhouhong07
  * @Date: 2019-09-03 18:01:11
  * @LastEditors: 
- * @LastEditTime: 2019-09-19 14:41:21
+ * @LastEditTime: 2019-09-19 18:35:52
  * @Description: file content
  */
 /**
@@ -47,9 +47,9 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {
 
 workbox.googleAnalytics.initialize({});
 
-// workbox.routing.registerRoute(
-//   /^https:\/\/rainbowzh.github.io\//i,
-//   workbox.strategies.networkFirst({
-//     cacheName: 'rainbow-change-cache'
-//   })
-// )
+workbox.routing.registerRoute(
+  /^https:\/\/rainbowzh.github.io\//i,
+  workbox.strategies.networkFirst({
+    cacheName: 'rainbow-change-cache'
+  })
+)
