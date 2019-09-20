5b79bf3bf09eca33f1a1dc51ba158e0837fb3107
zhouhong07 <zhouhong07@58ganji.com>
2019-09-20 11:00:17 +0800

    fix

--- static/precache-manifest.3277778a04fe07b218be2e650e681aec.js
+++ static/precache-manifest.3277778a04fe07b218be2e650e681aec.js
@@ -2,7 +2,7 @@
  * @Author: zhouhong07
  * @Date: 2019-09-03 18:01:11
  * @LastEditors: 
- * @LastEditTime: 2019-09-19 14:41:51
+ * @LastEditTime: 2019-09-20 10:59:39
  * @Description: file content
  */
 self.__precacheManifest = (self.__precacheManifest || []).concat([
@@ -23,5 +23,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     'url' : '/static/manifest.json'
+  },
+  {
+    "url" : 'https://j1.58cdn.com.cn/git/hrg-fe-zhaopin-common/Swiper-4.0/swiper.js'
   }
 ]);
